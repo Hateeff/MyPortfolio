a8127670ada449ac975db65a7985d82001734900
Hateef <hateef7788@gmail.com>
2026-04-08 18:06:36 +0300

    update: projects description content

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,9 +2,7 @@ import SaudiImprint from "@/assets/images/Saudi-imprint.png";
 import MyLibrary from "@/assets/images/mylibrary.png";
 import game from "@/assets/images/game.png";
 import Image from "next/image";
-import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 const portfolioProjects = [
@@ -13,9 +11,7 @@ const portfolioProjects = [
     year: "2025",
     title: "Saudi Imprint",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Saudi Imprint is an e-tourism platform that promotes tourism in Saudi Arabia by offering a digital guide to four main regions, featuring images and detailed descriptions of popular tourist destinations." },
     ],
     link: "https://github.com/Hateeff/saudi-imprint",
     image: SaudiImprint,
@@ -25,9 +21,7 @@ const portfolioProjects = [
     year: "2025",
     title: "Library Management System",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: "A web-based Library Management System built with PHP & MySQL, allowing administrators to manage books and users with CRUD operations and PDF support." },
     ],
     link: "https://github.com/Hateeff/Library-Management-System",
     image: MyLibrary,
@@ -37,9 +31,7 @@ const portfolioProjects = [
     year: "2026",
     title: "Tic Tac Toe AI Game",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Tic Tac Toe game with AI opponent built using Python and CustomTkinter, featuring multiple difficulty levels and a Minimax-based AI." },
     ],
     link: "https://github.com/Hateeff/tic-tac-toe-game",
     image: game,
@@ -74,7 +66,6 @@ export const ProjectsSection = () => {
       <ul className="flex flex-col gap-4 mt-4 md:mt-5">
         {project.results.map((result, index) => (
         <li key={index} className="flex gap-2 text-sm md:text-base text-white/50">
-          <CheckCircleIcon className="size-5 md:size-6"/>
         <span>{result.title}</span>
         </li>
         ))}
